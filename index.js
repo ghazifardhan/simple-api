e5e057d05f3ee2728050b2e1a4ba4296e590779b
myzpolice <ghazifardhan@gmail.com>
2016-12-19 13:52:56 +0700

    second commit

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
-var bodyparser = require('body-parser');
+var bodyParser = require('body-parser');
 var app = express();
 
 mongodb.connect('mongodb://localhost:27017/cermai', function(err, db) {
-	if(err) throw err;
+	if (err) throw err;
+
 	var ApiHandler = require('./routes/api');
 	var API = new ApiHandler(db);
 
 	app.use(bodyParser.json());
-	app.liste(2016, function() {
-		console.log("Application Listen on port", 2016)
+	app.use(bodyParser.urlencoded({ extended: false }));
+
+	app.get("/", API.index);
+	app.post("/api/auth", API.auth);
+
+	app.get("/api/item", API.isAuth, API.item);
+	app.get("/api/userInfo", API.isAuth, API.getInfo);
+
+	app.listen(2016, function() {
+		console.log("Application Listen on port ", 2016)
 	})
 })
-
-app.get("/", API.index);
-app.post("/api/auth", API.auth);
