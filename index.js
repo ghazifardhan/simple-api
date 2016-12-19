11bc39169473d72912c23ae40e648c3f1a581c7b
myzpolice <ghazifardhan@gmail.com>
2016-12-19 13:40:16 +0700

    edit

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ mongodb.connect('mongodb://localhost:27017/cermai', function(err, db) {
 	app.use(bodyParser.json());
 	app.liste(2016, function() {
 		console.log("Application Listen on port", 2016)
-	});
-});
+	})
+})
 
 app.get("/", API.index);
 app.post("/api/auth", API.auth);
