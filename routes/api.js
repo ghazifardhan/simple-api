5993067fc10d27aaf7332da3cb7fd0c7c599c46d
myzpolice <ghazifardhan@gmail.com>
2016-12-19 13:29:44 +0700

    adding auth

--- routes/api.js
+++ routes/api.js
@@ -4,3 +4,34 @@ function API(db) {
 	}
 }
 module.exports = API;
+
+this.auth = function(req,res,next) {
+        var username = req.body.username;
+    var password = req.body.password;
+    var ModelUser = db.collection('users');
+    var crypto = require('crypto');
+    var keyCrypto = "tangituru";
+    var moment = require('moment');
+
+    ModelUser.findOne({username : username, password : password}, function(err, hasil) {
+        if (err) throw err;
+        if (hasil) {
+            // PROSES CREATE TOKEN
+            var textToEncrpt = moment().format("YYYY-MM-DD HH:mm:ss") + "-" + username + "-" + password;
+            var token = crypto.createHmac('sha1', keyCrypto).update(textToEncrpt).digest("hex");
+
+            // PROSES SIMPAN TOKEN DI COLLECTIONS
+            var dataAuth = {
+                token : token,
+                created_at : moment().format("YYYY-MM-DD HH:mm:ss")
+            }
+            ModelUser.update({username : username, password : password}, {$set : {auth : dataAuth}}, function(err, hasilUpdate) {
+                if (err) throw err;
+                return res.json({status : 200, token : token})
+            })
+        }
+        else {
+            res.json({status : 403, msg : "Login Failed"})
+        }
+    })
+}
